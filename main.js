0e12b8cba667f6a4b0f6941a9ad5c9495b269bb6
Arnavkheterpal12 <71542068+Arnavkheterpal12@users.noreply.github.com>
2020-12-03 20:55:20 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+noseX=0;
+noseY=0;
 function preload(){
-
+clown_nose=loadImage('https://i.postimg.cc/8zwsgs39/Clipart-Key-760430.png');
 }
 function setup(){
     canvas=createCanvas(300,300);
@@ -13,7 +15,7 @@ function setup(){
 }
 function draw(){
   image(video,0,0,300,300);
-  
+  image(clown_nose,noseX,noseY,200,200);
 
 }
 function modelLoaded(){
@@ -22,8 +24,10 @@ function modelLoaded(){
 function gotPoses(results){
     if(results.length>0){
         console.log(results);
-        console.log("nose x="+results[0].pose.nose.x);
-        console.log("nose y="+results[0].pose.nose.y);
+       noseX= results[0].pose.nose.x-90;
+       noseY= results[0].pose.nose.y-120;
+        console.log("nose x="+noseX);
+        console.log("nose y="+noseY);
         
 
     }
